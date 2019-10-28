fdc3c0dc033579b3c96a9ac03a004ae3dd41be63
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-10-27 17:37:29 -0700

    frontend connect with backend through api call

--- frontend/nomz/src/App.js
+++ frontend/nomz/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const config = require("./config/config");
+
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {}
+  }
+
+  handleClick() {
+    console.log("Clicked!");
+    console.log(config.local_backend)
+    axios.get(config.local_backend + "/api/menuitems")
+          .then(resp => {console.log(resp.data); this.setState({menuItems: resp.data})});
+  }
+
+  render () {
+    return (
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick.bind(this)}>
+          Click Me
+        </button>
+        <p>menu items: {JSON.stringify(this.state.menuItems)}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
