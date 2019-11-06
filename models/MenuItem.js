4f904c53be8aaf40b576f91e521720fb8d120478
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-11-06 13:18:10 -0800

    fixed tests

--- models/MenuItem.js
+++ models/MenuItem.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // Create Schema
 
-/** 
+/**
  * @class MenuItem
  */
 const MenuItemSchema = new mongoose.Schema({
--- routes/api/menuItems.js
+++ routes/api/menuItems.js
@@ -12,7 +12,8 @@ router.get('/test', (req, res) => res.send('menuitem route testing!'));
 /**
  * Filters and retrives menu items.
  * @param {express.Request} req - The express request object.
- * @param {MenuItem} req.query - Filter containing properties of the desired menuitem. (Automatically filled by Express from URL params)
+ * @param {MenuItem} req.query - Filter containing properties of the desired menuitem.
+ * (Automatically filled by Express from URL params)
  * @param {express.Response} res - The express response object containing a list of menuitems.
  */
 function getMenuItems(req, res) {
@@ -25,7 +26,8 @@ router.get('/', getMenuItems);
 /**
  * Creates a menu item.
  * @param {express.Request} req - The express request object.
- * @param {MenuItem} req.body - The desired menuitem to create. (Automatically filled by Express from HTTP request body json)
+ * @param {MenuItem} req.body - The desired menuitem to create.
+ * (Automatically filled by Express from HTTP request body json)
  * @param {express.Response} res - The express response object incidating success or failure.
  */
 function createMenuItem(req, res) {
@@ -37,7 +39,7 @@ router.post('/', createMenuItem);
 
 /**
  * Updates a menu item.
- * @param {express.Request} req - The express request object. 
+ * @param {express.Request} req - The express request object.
  * @param {MenuItem} req.body.filter - The desired menuitem to update. Its
  * @param {MenuItem} req.body.update - The new properties to assign.
  * @param {express.Response} res - The express response object incidating success or failure.
@@ -55,7 +57,7 @@ router.put('/', updateMenuItem);
  * @param {MenuItem} req.body - The desired menuitem to delete.
  * @param {express.Response} res - The express response object incidating success or failure.
  */
- function deleteMenuItem(req, res) {
+function deleteMenuItem(req, res) {
   MenuItem.findOneAndDelete(req.body)
     .then(() => res.sendStatus(200))
     .catch((err) => { console.log(err); res.status(500).send(err); });
