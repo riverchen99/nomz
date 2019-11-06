267d27ffd1786550136360fb0d56f90035bb39d5
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-11-06 12:56:50 -0800

    add jsdoc

--- routes/api/menuItems.js
+++ routes/api/menuItems.js
@@ -5,36 +5,61 @@ const router = express.Router();
 // Load MenuItem model
 const MenuItem = require('../../models/MenuItem');
 
-// @route GET api/menuItems/test
-// @description test route
-// @access Public
+// test route
 router.get('/test', (req, res) => res.send('menuitem route testing!'));
 
-// @route GET api/menuItems
-// @description Get menuItems
-// @access Public
-router.get('/', (req, res) => {
+
+/**
+ * Filters and retrives menu items.
+ * @param {express.Request} req - The express request object.
+ * @param {MenuItem} req.query - Filter containing properties of the desired menuitem. (Automatically filled by Express from URL params)
+ * @param {express.Response} res - The express response object containing a list of menuitems.
+ */
+function getMenuItems(req, res) {
   MenuItem.find(req.query)
     .then((menuItems) => res.json(menuItems))
-    .catch((err) => { console.log(err); res.status(404).json({ noMenuItemsFound: 'No MenuItem found' }); });
-});
+    .catch((err) => { console.log(err); res.status(500).send(err); });
+}
+router.get('/', getMenuItems);
 
-router.post('/', (req, res) => {
+/**
+ * Creates a menu item.
+ * @param {express.Request} req - The express request object.
+ * @param {MenuItem} req.body - The desired menuitem to create. (Automatically filled by Express from HTTP request body json)
+ * @param {express.Response} res - The express response object incidating success or failure.
+ */
+function createMenuItem(req, res) {
   MenuItem.create(req.body)
     .then(() => res.sendStatus(200))
     .catch((err) => { console.log(err); res.status(500).send(err); });
-});
+}
+router.post('/', createMenuItem);
 
-router.put('/', (req, res) => {
+/**
+ * Updates a menu item.
+ * @param {express.Request} req - The express request object. 
+ * @param {MenuItem} req.body.filter - The desired menuitem to update. Its
+ * @param {MenuItem} req.body.update - The new properties to assign.
+ * @param {express.Response} res - The express response object incidating success or failure.
+ */
+function updateMenuItem(req, res) {
   MenuItem.findOneAndUpdate(req.body.filter, req.body.update)
     .then(() => res.sendStatus(200))
     .catch((err) => { console.log(err); res.status(500).send(err); });
-});
+}
+router.put('/', updateMenuItem);
 
-router.delete('/', (req, res) => {
+/**
+ * Deletes a menu item.
+ * @param {express.Request} req - The express request object.
+ * @param {MenuItem} req.body - The desired menuitem to delete.
+ * @param {express.Response} res - The express response object incidating success or failure.
+ */
+ function deleteMenuItem(req, res) {
   MenuItem.findOneAndDelete(req.body)
     .then(() => res.sendStatus(200))
     .catch((err) => { console.log(err); res.status(500).send(err); });
-});
+}
+router.delete('/', deleteMenuItem);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -21,11 +21,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-
+app.use('/docs', express.static('docs'));
 
 app.use('/api/menuItems', menuItemsRoute);
 app.get('/hello', (req, res) => res.send('Hello world!'));
-
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
