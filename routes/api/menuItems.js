64a3ff82d6e8ca1a36153702e55ebc01ff4eff78
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-11-07 20:51:48 -0800

    refactored controller methods

--- routes/api/menuItems.js
+++ routes/api/menuItems.js
@@ -1,67 +1,11 @@
 const express = require('express');
+const controller = require('../../controllers/menuItemController');
+
 
 const router = express.Router();
-
-// Load MenuItem model
-const MenuItem = require('../../models/MenuItem');
-
-// test route
-router.get('/test', (req, res) => res.send('menuitem route testing!'));
-
-
-/**
- * Filters and retrives menu items.
- * @param {express.Request} req - The express request object.
- * @param {MenuItem} req.query - Filter containing properties of the desired menuitem.
- * (Automatically filled by Express from URL params)
- * @param {express.Response} res - The express response object containing a list of menuitems.
- */
-function getMenuItems(req, res) {
-  MenuItem.find(req.query)
-    .then((menuItems) => res.json(menuItems))
-    .catch((err) => { console.log(err); res.status(500).send(err); });
-}
-router.get('/', getMenuItems);
-
-/**
- * Creates a menu item.
- * @param {express.Request} req - The express request object.
- * @param {MenuItem} req.body - The desired menuitem to create.
- * (Automatically filled by Express from HTTP request body json)
- * @param {express.Response} res - The express response object incidating success or failure.
- */
-function createMenuItem(req, res) {
-  MenuItem.create(req.body)
-    .then(() => res.sendStatus(200))
-    .catch((err) => { console.log(err); res.status(500).send(err); });
-}
-router.post('/', createMenuItem);
-
-/**
- * Updates a menu item.
- * @param {express.Request} req - The express request object.
- * @param {MenuItem} req.body.filter - The desired menuitem to update. Its
- * @param {MenuItem} req.body.update - The new properties to assign.
- * @param {express.Response} res - The express response object incidating success or failure.
- */
-function updateMenuItem(req, res) {
-  MenuItem.findOneAndUpdate(req.body.filter, req.body.update)
-    .then(() => res.sendStatus(200))
-    .catch((err) => { console.log(err); res.status(500).send(err); });
-}
-router.put('/', updateMenuItem);
-
-/**
- * Deletes a menu item.
- * @param {express.Request} req - The express request object.
- * @param {MenuItem} req.body - The desired menuitem to delete.
- * @param {express.Response} res - The express response object incidating success or failure.
- */
-function deleteMenuItem(req, res) {
-  MenuItem.findOneAndDelete(req.body)
-    .then(() => res.sendStatus(200))
-    .catch((err) => { console.log(err); res.status(500).send(err); });
-}
-router.delete('/', deleteMenuItem);
+router.get('/', controller.getMenuItems);
+router.post('/', controller.createMenuItem);
+router.put('/', controller.updateMenuItem);
+router.delete('/', controller.deleteMenuItem);
 
 module.exports = router;
