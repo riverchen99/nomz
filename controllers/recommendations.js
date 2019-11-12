467f22fea7b16e4cd105f50c0c43db8f6f717b2e
Samiha Rahman <samiha.rahman@ucla.edu>
2019-11-12 14:58:26 -0800

    Extract MenuItems form Menus of current time frame

--- controllers/recommendations.js
+++ controllers/recommendations.js
@@ -1,13 +1,25 @@
 const MenuItem = require('../models/MenuItem');
+const Menu = require('../models/Menu');
 /*
 const Restaurant = require('../models/Restaurant');
-const Menu = require('../models/Menu');
+
 const Review = require('../models/Review');
 const User = require('../models/User');
 */
 
+function generateRecommendations(availableMenuItemIds) {
+  // look up menuItem by menuItemIds
+  console.log(availableMenuItemIds);
+  MenuItem.find({'_id': { $in: availableMenuItemIds}})
+  .then((results) =>{
+    console.log(results)
+  })
+  .catch((err) => { console.log(err); res.status(500).send(err); });
+  // do the algorithm
+}
+
 function getRecommendations(req, res) {
-  MenuItem.find({})// req.query}) // empty or userId = .....
+  /* MenuItem.find({})// req.query}) // empty or userId = .....
   .then((results) => {
       // change this code
       results.sort((a, b) => b.rating - a.rating);
@@ -16,25 +28,22 @@ function getRecommendations(req, res) {
       res.json(recommendations);
     })
     .catch((err) => { console.log(err); res.status(500).send(err); });
-   const currentDateFilter = {}; // fill this in
-   Menu.find(currentDateFilter)
+   */
+
+  // const currentDateFilter = {}; // fill this in
+  const date = new Date('Tue Nov 12 2019 11:30:00 AM');
+  (Menu.find({ endTime: { $gte: date } })).find({ startTime: { $lte: date } })
     .then((results) => {
       // results is an array of Menus available today
-
-
-      menuItemIds = []
-      for (var i = 0; i < results.length; i++) {
-        menuItemIds.concat(results[i])
+      // console.log(results);
+      let menuItemIds = [];
+      for (let i = 0; i < results.length; i += 1) {
+        menuItemIds = menuItemIds.concat((results[i]).menuItems);
       }
-
-      res.json(generateRecommendations(menuItemIds))
-    })
+      console.log(menuItemIds);
+      res.json(generateRecommendations(menuItemIds));
+    });
 }
 
-
-function generateRecommendations(availableMenuItemIds) {
-    // look up menuItem by menuItemIds
-    // do the algorithm
-}
 
 module.exports = { getRecommendations };
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const MenuItem = require('../models/MenuItem');
+const Menu = require('../models/Menu');
+const Restaurant = require('../models/Restaurant');
 // const Recommendations = require('../controllers/recommendations');
 const { app, runServer, closeServer } = require('../server');
 
@@ -13,19 +15,64 @@ try {
   TEST_DB_URI = process.env.MONGODB_TEST_URI;
 }
 
+// let menuItemsIds = [];
+
+const item1 = new MenuItem({ name: 'combo', rating: 3 });
+const item2 = new MenuItem({ name: 'fish and chips', ingredients: ['fish', 'potato'], rating: 4 });
+const item3 = new MenuItem({ name: 'spaghetti', ingredients: ['pasta', 'tomato', 'oregano'], rating: 5 });
+const item4 = new MenuItem({ name: 'Veggies', rating: 2 });
+const item5 = new MenuItem({ name: 'Steak', ingredients: ['Beef', 'potato'], rating: 5 });
+const item6 = new MenuItem({ name: 'lentil spaghetti', ingredients: ['pasta', 'lentil', 'oregano'], rating: 5 });
+const item7 = new MenuItem({ name: 'Eggs', rating: 3 });
+const item8 = new MenuItem({ name: 'Bacon', ingredients: ['Bacon'], rating: 5 });
+const item9 = new MenuItem({ name: 'Breakfast Taco', ingredients: ['tortilla'], rating: 5 });
+
 function seedMenuItemData() {
-  return MenuItem.insertMany([
-    { name: 'combo', rating: 3 },
-    { name: 'fish and chips', ingredients: ['fish', 'potato'], rating: 4 },
-    { name: 'spaghetti', ingredients: ['pasta', 'tomato', 'oregano'], rating: 5 },
+  MenuItem.insertMany([
+    item1,
+    item2,
+    item3,
+    item4,
+    item5,
+    item6,
+    item7,
+    item8,
+    item9,
   ]);
+}
+
+function seedMenuData() {
+  return Menu.insertMany(
+    [
+      {
+        restaurant: new Restaurant({ name: 'Covel' }),
+        startTime: new Date('Tue Nov 12 2019 11:00 AM'),
+        endTime: new Date('Tue Nov 12 2019 2:00 PM'),
+        menuItems: [item1.id, item2.id, item3.id],
+      },
+
+      {
+        restaurant: new Restaurant({ name: 'BPlate' }),
+        startTime: new Date('Tue Nov 12 2019 11:00 AM'),
+        endTime: new Date('Tue Nov 12 2019 2:00 PM'),
+        menuItems: [item4.id, item5.id, item6.id],
+      },
+
+      {
+        restaurant: new Restaurant({ name: 'BPlate' }),
+        startTime: new Date('Tue Nov 12 2019 7:00 AM'),
+        endTime: new Date('Tue Nov 12 2019 9:00 AM'),
+        menuItems: [item7.id, item8.id, item9.id],
+      },
+    ],
+  );
 }
 
 function tearDownDb() {
   console.warn('Deleting database');
   return mongoose.connection.dropDatabase();
 }
-
+/*
 describe('MenuItems API works correctly', () => {
   beforeAll(() => runServer(TEST_DB_URI));
 
@@ -39,7 +86,8 @@ describe('MenuItems API works correctly', () => {
     it('should return 200 and nonempty list', async () => {
       const res = await request(app).get('/api/menuitems');
       expect(res.status).toBe(200);
-      // expect(res.body).toHaveLength(3);
+      expect(res.body).toHaveLength(9);
+      console.log(res.body);
     });
   });
 
@@ -48,7 +96,7 @@ describe('MenuItems API works correctly', () => {
       const res = await request(app).post('/api/menuitems').send({ name: 'new item' });
       expect(res.status).toBe(200);
       const menuItems = await MenuItem.find();
-      expect(menuItems).toHaveLength(4);
+      expect(menuItems).toHaveLength(10);
     });
   });
 
@@ -71,15 +119,17 @@ describe('MenuItems API works correctly', () => {
       const res = await request(app).delete('/api/menuitems').send({ name: 'fish and chips' });
       expect(res.status).toBe(200);
       const menuItems = await MenuItem.find();
-      expect(menuItems).toHaveLength(2);
+      expect(menuItems).toHaveLength(9);
     });
   });
-});
+}); */
 
 describe('Recommendations API works correctly', () => {
   beforeAll(() => runServer(TEST_DB_URI));
 
   beforeEach(() => seedMenuItemData());
+
+  beforeEach(() => seedMenuData());
 
   afterEach(() => tearDownDb());
 
