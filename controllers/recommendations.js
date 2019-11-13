bb6bbef2e47b4e449804ba78bfa6263c1df526e2
Samiha Rahman <samiha.rahman@ucla.edu>
2019-11-13 12:30:59 -0800

    Commented out user filter

--- controllers/recommendations.js
+++ controllers/recommendations.js
@@ -7,6 +7,155 @@ const Restaurant = require('../models/Restaurant');
 const Review = require('../models/Review');
 const User = require('../models/User');
 */
+/*function preferenceMatchScore(userInfo, menuItem){
+
+}*/
+
+function propsCheck(infoArray, props, type){
+  var matchScore = 0;
+  const maxScore = infoArray.length;
+  if (maxScore == 0){
+    //no restrictions/preferences to worry about so we can include it
+    return true;
+  }
+  for (var i = 0; i < inforArray.length; i+=1){
+    switch(infoArray[i]){
+      case('vegetarian'):
+      {
+        if (props.vegetarian){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('vegan'):
+      {
+        if (props.vegan){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('peanuts'):
+      {
+        if (props.peanuts){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('treeNuts'):
+      {
+        if (props.treeNuts){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('wheat'):
+      {
+        if (props.wheat){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('gluten'):
+      {
+        if (props.gluten){
+          matchScore+=1;
+        }        
+        break;
+      }return true;
+      case('soy'):
+      {
+        if (props.soy){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('dairy'):
+      {
+        if (props.dairy){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('eggs'):
+      {
+        if (props.eggs){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('shellfish'):
+      {
+        if (props.shellfish){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('fish'):
+      {
+        if (props.fish){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('halal'):
+      {
+        if (props.halal){
+          matchScore+=1;
+        }        
+        break;
+      }
+      case('lowCarbon'):
+      {
+        if (props.lowCarbon){
+          matchScore+=1;
+        }        
+        break;
+      }
+    }
+  }
+  if (type == 'restrictions'){
+    if (matchScore == 0){
+      //contains no "bad" stuff, include in filtered results
+      return true; 
+    } else {
+      //contains incompatible stuff, exclude from filtered results
+      return false; 
+    }
+  } else if (type == 'preferences'){
+    //80% matching threshold for acceptance of match 
+    if ((matchScore/maxScore) >= 0.5){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+//Used by both ingredients and allergens
+function restrictionCheck(restrictions, info){
+  //var infoArray = info.split(", ");
+  if (restrictions.length == 0 || info.length == 0){
+    //no restrictions, good to go
+    return true;
+  }
+
+  for (var i = 0; i < restrictions.length; i+=1){
+    if (infoArray.includes(restrictions[i])){
+      //Exclude from filtered results
+      return false;
+    }
+  }
+  //Include in filtered results
+  return true;
+}
+
+function itemCompatibilty(preferences, restrictions, menuItem){
+  var prefProps = (propsCheck(preferences, menuItem.props, "preferences"));
+  var restrictProps = (propsCheck(restrictions, menuItem.props, "restrictions"));
+  var ingredientCheck = restrictionCheck(restrictions, menuItem.ingredients);
+  var allergenCheck = restrictionCheck(restrictions, menuItem.allergens);
+  return prefProps && restrictProps && ingredientCheck && allergenCheck;
+}
 
 function generateRecommendations(availableMenuItemIds,
   filterRestaurant,
