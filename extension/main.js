f1faf3102931d81af8ee930f7c87cf9a1bf5986c
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-11-12 20:01:48 -0800

    added view star rating functionality

--- extension/main.js
+++ extension/main.js
@@ -1,8 +1,43 @@
-menuItems = document.getElementsByClassName("menu-item");
+const menuItems = $('.menu-item');
 
-console.log(menuItems)
+console.log(menuItems);
 
+const remoteURL = 'http://cs130-nomz.herokuapp.com'; // eslint-disable-line
+const localURL = 'http://localhost:8080';
+const itemIds = [];
+
+menuItems.each((index, item) => {
+  console.log($(item).find('.recipelink').attr('href'));
+  const itemId = $(item).find('.recipelink').attr('href').split('/')
+    .slice(-2, -1)[0];
+  itemIds.push(itemId);
+});
+
+console.log(itemIds);
+
+
+$.get(`${localURL}/api/menuItems/${itemIds.join(';')}`, (data) => {
+  console.log(data);
+  console.log($(`a[href*="${data[0]._id}"]`));
+
+  for (let i = 0; i < data.length; i += 1) {
+    const aTag = $(`a[href*="${data[i]._id}"]`);
+    if (aTag.length !== 0) {
+      const ratingDiv = aTag.after(`<div class="star-rating" id="star-rating-${data[i]._id}"></div>`);
+      console.log(ratingDiv);
+      ratingDiv.starRating({
+        initialRating: data[i].rating,
+        starSize: 10,
+        starShape: 'rounded',
+      });
+    }
+  }
+});
+
+
+/*
 for (const menuItem of Object.values(menuItems)) {
-	var text = document.createTextNode("5 stars");
-	menuItem.appendChild(text);
+  var text = document.createTextNode("5 stars");
+  menuItem.appendChild(text);
 }
+*/
--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-let loginButton = document.getElementById("loginButton");
-loginButton.onclick = function(element) {
-	// ...
-}
+const loginButton = document.getElementById('loginButton');
+loginButton.onclick = function () { // can take function(element), removing for now to fix lint
+  // ...
+};
