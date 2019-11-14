5b75a9bed6b69817d1f7742f17785a02f1a7f5b6
Ben L <ben.limpanukorn@gmail.com>
2019-11-14 15:47:06 -0800

    map scraped data to schema

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-// const Menu = require('../models/Menu');
-// const MenuItem = require('../models/MenuItem');
-// const Restaurant = require('../models/Restaurant');
+const Menu = require('../models/Menu');
+const MenuItem = require('../models/MenuItem');
+const Restaurant = require('../models/Restaurant');
 
 const menuUrl = (date) => `http://menu.dining.ucla.edu/Menus/${date}`;
 const recipeUrl = (recipeId, recipeSize) => `http://menu.dining.ucla.edu/Recipes/${recipeId}/${recipeSize}`;
@@ -29,7 +29,7 @@ const PROPS_MAPPING = {
   LC: 'lowCarbon',
 };
 
-const getRecipe = async (recipeId, recipeSize) => {
+const fetchRecipeData = async (recipeId, recipeSize) => {
   const $ = await fetchData(recipeUrl(recipeId, recipeSize));
   const name = $('h2').text().trim();
   // main props
@@ -71,7 +71,7 @@ const getRecipe = async (recipeId, recipeSize) => {
   return info;
 };
 
-const getMenu = async (date) => {
+const fetchMenuData = async (date) => {
   const $ = await fetchData(menuUrl(date));
 
   const items = [];
@@ -115,31 +115,97 @@ const getMenu = async (date) => {
   return items;
 };
 
-const updateMenuDay = async (date) => {
-  const items = await getMenu(date);
-  let recipes = [];
-  for (let i = 0; i < items.length; i += 1) {
-    const item = items[i];
-    recipes.push(getRecipe(item.recipeId, item.recipeSize));
-  }
-  recipes = await Promise.all(recipes);
-
-  for (let i = 0; i < items.length; i += 1) {
-    // const item = items[i];
-    // const recipe = recipes[i];
-  }
+const fetchMenuTimes = async (date) => {
+  // TODO: scrape the menu times from the web page
+  const dateArr = date.split('-');
+  return {
+    Breakfast: {
+      start: new Date(...dateArr, 6),
+      end: new Date(...dateArr, 9),
+    },
+    Brunch: {
+      start: new Date(...dateArr, 9),
+      end: new Date(...dateArr, 14),
+    },
+    Lunch: {
+      start: new Date(...dateArr, 11),
+      end: new Date(...dateArr, 14),
+    },
+    Dinner: {
+      start: new Date(...dateArr, 17),
+      end: new Date(...dateArr, 20),
+    },
+  };
 };
+
+const getUpdatedMenu = async (date) => {
+  let menuItems = [];
+  let restaurants = {};
+  let menus = {};
+
+  await fetchMenutimes(date).then((times) => {
+    fetchMenuData(date).then((items) => {
+      for (let i = 0; i < items.length; i += 1) {
+        const item = items[i];
+
+        // create restaurant
+        if (item.diningHall in restaurants) {
+          restaurants['']
+        } else {
+          restaurants.push(Restaurant.create({name: item.diningHall, menus:}))
+        }
+
+        // create menu
+        if (item.menuPeriod in menus) {
+          menus[item.menuPeriod].menuItems.push(item.recipeId);
+        } else {
+          menus[item.menuPeriod] = Menu.create({
+            mealPeriod: item.menuPeriod,
+            startTime: times[item.menuPeriod].startTime,
+            endTime: times[item.menuPeriod].endTime,
+            restaurant: item.diningHall,
+            menuItems: [item.recipeId],
+          });
+        }
+
+
+        // create menu item
+        fetchRecipeData(item.recipeId, item.recipeSize).then((recipe) => {
+          menuItems.push(MenuItem.create({
+            _id: item.recipeId,
+            name: recipe.name,
+            rating: null,
+            description: recipe.description,
+            ingredients: recipe.ingredients,
+            allergens: recipe.allergens,
+            props: recipe.props,
+            restaurant: item.diningHall,
+            station: item.diningSection,
+          }));
+        });
+      }
+    });
+  });
 
 const testFunction = async () => {
   // const items = await getMenu('2019-11-13');
   // console.log(items);
   // const test = await getRecipe('031002', '5');
   // await updateMenuDay('2019-11-13');
+  // MenuItem.find({}).then((result) => console.log(result));
+  // MenuItem.findById('047073').then((result) => {
+  //   if (result === null) {
+  //     console.log('result is null');
+  //   } else {
+  //     console.log(result);
+  //   }
+  // });
+  const dayMenu = await getUpdatedMenu('2019-11-13');
+  for (let i = 0; i < dayMenu.menuItems.length; i += 1) {
+    dayMenu.menuItems[i].save()
+  }
 };
 
 module.exports = {
-  getMenu,
-  getRecipe,
   test: testFunction,
-  updateMenuDay,
 };
