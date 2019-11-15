559e151dc62d0e1c707aa8f8b33fb2c882e9ed6d
Ben L <ben.limpanukorn@gmail.com>
2019-11-14 19:41:11 -0800

    Comments + JSDoc

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,3 +1,7 @@
+/**
+ @module scraper
+ */
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -5,14 +9,7 @@ const Menu = require('../models/Menu');
 const MenuItem = require('../models/MenuItem');
 const Restaurant = require('../models/Restaurant');
 
-const menuUrl = (date) => `http://menu.dining.ucla.edu/Menus/${date}`;
-const recipeUrl = (recipeId, recipeSize) => `http://menu.dining.ucla.edu/Recipes/${recipeId}/${recipeSize}`;
-
-const fetchData = async (url) => {
-  const result = await axios.get(url);
-  return cheerio.load(result.data);
-};
-
+// the key for the alt text of the special property icons
 const PROPS_MAPPING = {
   V: 'vegetarian',
   VG: 'vegan',
@@ -29,9 +26,36 @@ const PROPS_MAPPING = {
   LC: 'lowCarbon',
 };
 
+const menuUrl = (date) => `http://menu.dining.ucla.edu/Menus/${date}`;
+const recipeUrl = (recipeId, recipeSize) => `http://menu.dining.ucla.edu/Recipes/${recipeId}/${recipeSize}`;
+
+
+/**
+ * Helper function to fetch a webpage and pass it to cheerio for parsing.
+ *
+ * @param {String} url - The URL of the page to fetch
+ * @return {cheerio} - Returns an instance of cheerio that can be used like jQuery
+ * to select and filter elements.
+ */
+const fetchData = async (url) => {
+  const result = await axios.get(url);
+  return cheerio.load(result.data);
+};
+
+/**
+ * Helper function to fetch and parse the recipe data of a menu item.
+ *
+ * @param {String} recipeId - The recipe ID of the menu item.
+ * @param {String} recipeSize - The typical serving size of the menu item.
+ * @return {Object} - Returns an recipe object containing information scraped
+ * from the recipe page.
+ */
 const fetchRecipeData = async (recipeId, recipeSize) => {
   const $ = await fetchData(recipeUrl(recipeId, recipeSize));
   const name = $('h2').text().trim();
+
+  // some menu items do not have recipe pages
+  // in this case we return an empty object
   if (name === '') {
     return {
       name: '',
@@ -55,13 +79,18 @@ const fetchRecipeData = async (recipeId, recipeSize) => {
       },
     };
   }
-  // main props
+
+  // main properties of the item
   const ingredientsEle = $('div.ingred_allergen').children();
+  const description = $('div.description').text().trim();
+
+  // the ingredients and allergens may sometimes be missing
   const ingredients = (ingredientsEle.length > 0)
     ? $(ingredientsEle[0]).text().split(': ')[1].split(', ') : '';
   const allergens = (ingredientsEle.length > 1)
     ? $(ingredientsEle[1]).text().split(': ')[1].split(', ') : '';
-  // special properties
+
+  // special properties of the item
   const props = {
     vegetarian: false,
     vegan: false,
@@ -77,12 +106,14 @@ const fetchRecipeData = async (recipeId, recipeSize) => {
     halal: false,
     lowCarbon: false,
   };
+
+  // the alt text tells us what property it is
   $('div.productinfo').find('img').each((_i, imgEle) => {
     props[PROPS_MAPPING[$(imgEle).attr('alt')]] = true;
   });
-  const description = $('div.description').text().trim();
 
   // nutrition information
+  // TODO stretch goal is to get the nutrition information
   // const nutrition = {};
 
   const info = {
@@ -95,6 +126,14 @@ const fetchRecipeData = async (recipeId, recipeSize) => {
   return info;
 };
 
+/**
+ * Helper function to fetch and parse the menu data for a particular day.
+ *
+ * @param {String} date - The ISO format date (YYYY-MM-DD)
+ * @return {Array} - Returns an array of menu item objects. Each object contains
+ * basic information that can be used to fetch the full recipe information with
+ * fetchRecipeData.
+ */
 const fetchMenuData = async (date) => {
   const $ = await fetchData(menuUrl(date));
 
@@ -103,9 +142,12 @@ const fetchMenuData = async (date) => {
   const headers = $('#page-header');
   for (let i = 0; i < headers.length; i += 1) {
     // find the menu date
+    // The date string is: "PERIOD Menu for DAY, DATE"
     const menuDateStr = $(headers[i]).text();
     const menuPeriod = menuDateStr.split(' ')[0];
     const menuDate = menuDateStr.substring(2 + menuDateStr.indexOf(',')).trim();
+
+    // we only want the menu items for this period
     const elementsBetween = $(headers[i]).nextUntil(headers[i + 1]);
     const menuBlocks = elementsBetween.filter('div.menu-block.half-col');
 
@@ -117,12 +159,16 @@ const fetchMenuData = async (date) => {
       // find the dining hall section
       const diningSectionsEle = diningHallEle.nextAll().filter('ul.sect-list').children();
       $(diningSectionsEle).each((_i1, diningSectionEle) => {
+        // the text returned contains all of the sub elements, we need to get only
+        // the first line.
+        // Sometimes the first line has a leading newline so we trim first
         const fullDiningText = $(diningSectionEle).text().trim();
         const diningSection = fullDiningText.substring(0, fullDiningText.indexOf('\n'));
 
         // find the menu items
         $(diningSectionEle).find('li.menu-item').each((_i2, menuItemEle) => {
           const menuItemLinkEle = $(menuItemEle).find('a.recipelink');
+          // the recipe id and serving size are contained in the URL
           const recipeId = menuItemLinkEle.attr('href').split('/')[4];
           const recipeSize = menuItemLinkEle.attr('href').split('/')[5];
           const name = menuItemLinkEle.text();
@@ -136,6 +182,13 @@ const fetchMenuData = async (date) => {
   return items;
 };
 
+/**
+ * Helper function to fetch and parse the meal periods for a particular day.
+ * This function currently has a dummy implementation.
+ *
+ * @param {String} date - The ISO format date (YYYY-MM-DD)
+ * @return {Object} - Returns an object mapping each meal period to a start and end time
+ */
 const fetchMenuTimes = async (date) => {
   // TODO: scrape the menu times from the web page
   const dateArr = date.split('-');
@@ -159,12 +212,23 @@ const fetchMenuTimes = async (date) => {
   };
 };
 
+/**
+ * Function to collate the parsed data and map it to our database schema.
+ *
+ * @param {String} date - The ISO format date (YYYY-MM-DD)
+ * @param {Function} fetchMenu - A function to retrieve the menu data.
+ * @param {Function} fetchTimes - A function to retrieve the meal period data.
+ * @param {Function} fetchRecipes - A function to retrieve the recipe data.
+ * @return {Object} - Returns an object encapsulating the fully collated data in
+ * the form of MenuItem, Menu, and Restaurant Documents.
+ */
 const getUpdatedMenu = async (
   date,
   fetchMenu = fetchMenuData,
   fetchTimes = fetchMenuTimes,
   fetchRecipes = fetchRecipeData) => {
   const menuItems = [];
+  // the restaurants must already exist in the database
   const restaurants = {
     Covel: await Restaurant.findOne({ name: 'Covel' }),
     'Bruin Plate': await Restaurant.findOne({ name: 'Bruin Plate' }),
@@ -176,11 +240,13 @@ const getUpdatedMenu = async (
   const times = await fetchTimes(date);
   const items = await fetchMenu(date);
 
+  // We asynchronously begin fetching the recipe data
   const recipePromises = [];
   for (let i = 0; i < items.length; i += 1) {
     const item = items[i];
     recipePromises.push(fetchRecipes(item.recipeId, item.recipeSize));
   }
+  // now we wait for our recipes to finish fetching and parsing
   const recipes = await Promise.all(recipePromises);
 
   for (let i = 0; i < items.length; i += 1) {
@@ -188,12 +254,15 @@ const getUpdatedMenu = async (
     const recipe = recipes[i];
 
     // update restaurant
+    // TODO update the restaurant documents
     // restaurants[item.diningHall](Restaurant.create({name: item.diningHall, menus:}))
 
     // create menu
     if (item.menuPeriod in menus) {
+      // the menu doc already exists so we just append to it
       menus[item.menuPeriod].menuItems.push(item.recipeId);
     } else {
+      // we need to create a new menu doc
       menus[item.menuPeriod] = new Menu({
         mealPeriod: item.menuPeriod,
         startTime: times[item.menuPeriod].startTime,
@@ -226,6 +295,9 @@ const getUpdatedMenu = async (
 
 module.exports = {
   getUpdatedMenu,
+
+  // these should be private
+  // they are only exported so that we can test it with Jest
   fetchMenuData,
   fetchRecipeData,
   fetchMenuTimes,
