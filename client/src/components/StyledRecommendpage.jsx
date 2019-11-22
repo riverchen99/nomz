6af0ad3fd64b81dbc3f9398f36ca04c70df9b056
grace-y728 <grace.nq.yu98@gmail.com>
2019-11-22 11:43:26 -0800

    some styling changes for improved usability

--- client/src/components/StyledRecommendpage.jsx
+++ client/src/components/StyledRecommendpage.jsx
@@ -11,14 +11,30 @@ export const Header = styled.h1`
 
 export const Text = styled.h3`
   color: #888888;
+  font-size: 24px;
 `;
 
 export const FilterContainer = styled.div`
   background: #FCE3FF;
-  padding: 2% 5%;
+  padding: 2% 15%;
 `;
 
-export const DropdownContainer = styled.div`
-  height: 10%;
-  width: 20%;
+export const ButtonContainer = styled.div`
+  margin-left: 5%;
+  float: right;
+`;
+
+export const FloatRightContainer = styled.div`
+  width: 30%;
+  margin-left: 15%;
+  float: right;
+`;
+export const AFloatRightContainer = styled.div`
+  width: 100%;
+  float: right;
+`;
+
+export const Row = styled.div`
+  width: 50%;
+  margin: 5px 0px;
 `;
--- client/src/options.js
+++ client/src/options.js
@@ -11,3 +11,35 @@ export const dayOptions = [
   { value: 'tomorrow', label: 'Tomorrow' },
 ];
 export const dayDefaultOption = dayOptions[0];
+
+export const timeOptions = [
+  { value: '7:00', label: '7:00 AM' },
+  { value: '7:30', label: '7:30 AM' },
+  { value: '8:00', label: '8:00 AM' },
+  { value: '8:30', label: '8:30 AM' },
+  { value: '9:00', label: '9:00 AM' },
+  { value: '9:30', label: '9:30 AM' },
+  { value: '10:00', label: '10:00 AM' },
+  { value: '10:30', label: '10:30 AM' },
+  { value: '11:00', label: '11:00 AM' },
+  { value: '11:30', label: '11:30 AM' },
+  { value: '12:00', label: '12:00 PM' },
+  { value: '12:30', label: '12:30 PM' },
+  { value: '1:00', label: '1:00 PM' },
+  { value: '1:30', label: '1:30 PM' },
+  { value: '2:00', label: '2:00 PM' },
+  { value: '2:30', label: '2:30 PM' },
+  { value: '3:00', label: '3:00 PM' },
+  { value: '3:30', label: '3:30 PM' },
+  { value: '4:00', label: '4:00 PM' },
+  { value: '4:30', label: '4:30 PM' },
+  { value: '5:00', label: '5:00 PM' },
+  { value: '5:30', label: '5:30 PM' },
+  { value: '6:00', label: '6:00 PM' },
+  { value: '6:30', label: '6:30 PM' },
+  { value: '7:00', label: '7:00 PM' },
+  { value: '7:30', label: '7:30 PM' },
+  { value: '8:00', label: '8:00 PM' },
+
+];
+export const timeDefaultOption = timeOptions[0];
