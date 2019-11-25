f355e20c661b65f639bcdb4793b910a4b32a6926
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-11-25 00:33:44 -0800

    work on chrome extension login

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+});
+
+
 const loginButton = document.getElementById('loginButton');
 loginButton.onclick = function () { // can take function(element), removing for now to fix lint
   // ...
 };
+
+
+$.get("http://localhost:8080/auth/user", function (data) {
+	console.log(data)
+})
+
+
+/*
+axios.get('/auth/user').then(response => {
+      console.log(response.data)
+      if (!!response.data.user) {
+        this.setState({
+          loggedIn: true,
+          user: response.data.user
+        })
+      } else {
+        this.setState({
+          loggedIn: false,
+          user: null
+        })
+      }
+    })
+    */
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const path = require('path');
 const loginController = require('../controllers/login'); // eslint-disable-line
 
 const CLIENT_HOME_PAGE_URL = '/recommend';
@@ -27,20 +28,50 @@ router.post('/logout', (req, res) => {
   return res.json({ msg: 'no user to log out!' });
 });
 
-router.get('/facebook', passport.authenticate('facebook'));
+router.get('/facebook', function (req, res, next) {
+  passport.authenticate('facebook', { callbackURL: "/auth/facebook/callback" + (req.query.extension ? "?extension=1" : "") })(req, res, next);
+});
 
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
-/*
-router.get('/facebook/callback',
-  passport.authenticate('facebook'),
+
+router.get("/loginSuccess", (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'extension', 'loggedIn.html'));
+});
+
+router.get("/facebook/callback",
+  function(req, res, next) {
+    passport.authenticate("facebook", {
+      callbackURL: "/auth/facebook/callback" + (req.query.extension ? "?extension=1" : ""),
+    })(req, res, next)
+  },
   function(req, res) {
-    res.redirect(CLIENT_HOME_PAGE_URL + "?id=" + req.user.id);
+    console.log("extension: " + req.query.extension);
+    res.redirect("/auth/loginSuccess")
+  }
+);
+
+/*
+router.get('/facebook/callback', function(req, res, next) {
+  passport.authenticate('facebook', {}
+}
+  ,
+  function(req, res) {
+    console.log(req.query);
+    if (req.query.extension === 1) {
+      console.log('chrome login');
+      res.redirect("/auth/loginSuccess")
+    } else {
+      console.log("regular login");
+      res.redirect(CLIENT_HOME_PAGE_URL)
+    }
+    //res.redirect(CLIENT_HOME_PAGE_URL + "?id=" + req.user.id);
   }
 );
 */
+/*
 router.get(
   '/facebook/callback',
   passport.authenticate('facebook', {
@@ -48,5 +79,5 @@ router.get(
     // failureRedirect: '/login',
   }),
 );
-
+*/
 module.exports = router;
