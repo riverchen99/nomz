a7a62e54cdba91e6c42f8c06fa7c7bee2d6be84b
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-11-25 20:13:34 -0800

    try https flag

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const path = require('path');
+//const path = require('path');
 const loginController = require('../controllers/login'); // eslint-disable-line
 
 const CLIENT_HOME_PAGE_URL = '/recommend';
