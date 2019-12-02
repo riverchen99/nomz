3db24b2f2205eca2f15a740b1ebbd286ef4e0cec
grace-y728 <grace.nq.yu98@gmail.com>
2019-12-01 23:23:45 -0800

    bug fix

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -13,16 +13,13 @@ class NavBar extends React.Component {
     }
   }
   render() {
-    let lastLinks;
-    if (this.state.loggedIn) {
+    let lastLinks = <div></div>;
+    if (this.props.userName !== "Guest") {
       lastLinks = (
-        <React.Fragment>
-          <Item><Link to={{ pathname: '/profile'}} style={{ textDecoration: 'none', color: '#888888' }}>Profile</Link></Item>
-          <Item><a href="/auth/logout">Logout</a></Item>
-        </React.Fragment>);
-    }
-    else {
-      lastLinks = <div></div>;
+      <React.Fragment>
+        <Item><Link to={{ pathname: '/profile'}} style={{ textDecoration: 'none', color: '#888888' }}>Profile</Link></Item>
+        <Item><a href="/auth/logout">Logout</a></Item>
+      </React.Fragment>);
     }
     return (
       <Wrapper>
@@ -34,7 +31,7 @@ class NavBar extends React.Component {
         <Menu expanded={this.state.expanded} >
           <Item><Link to={{ pathname: '/recommend'}} style={{ textDecoration: 'none', color: '#888888' }}>Top Rated</Link></Item>
           <Item><Link to={{ pathname: '/allmenus' }} style={{ textDecoration: 'none',  color: '#888888' }}>Menus</Link></Item>
-          {lastLinks}
+        {lastLinks}
         </Menu>
       </Wrapper>
     )
